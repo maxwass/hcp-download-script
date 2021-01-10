60f8e372a5317d3f6765aa461c09b3cb39831262
Max Wasserman <maxw14k@gmail.com>
2021-01-10 15:53:00 -0500

    README included with instructions on how to run. script cleaned and clarified how to include new files in download

--- batch_download.py
+++ batch_download.py
@@ -2,6 +2,8 @@
 # AWS S3 high level api:
 #   https://docs.aws.amazon.com/cli/latest/userguide/cli-services-s3-commands.html
 
+# search for CHANGE for locations of where to customize for new files
+
 import os
 import time
 import subprocess
@@ -11,7 +13,8 @@ from scipy.io import savemat
 
 encoding = 'utf-8'
 
-#relative file path for each patient
+############
+#relative file path for each patient: CHANGE
 lr_msmall = "rfMRI_REST1_LR_Atlas_MSMAll_hp2000_clean.dtseries.nii"
 rl_msmall = "rfMRI_REST1_RL_Atlas_MSMAll_hp2000_clean.dtseries.nii"
 
@@ -28,7 +31,7 @@ rl_no_msmall_hcp_rel_path = "/MNINonLinear/Results/rfMRI_REST1_RL/" + rl_no_msma
 
 
 atlas_file_pre  = "/MNINonLinear/fsaverage_LR32k/"
-detrieux_atlas_file_post = ".aparc.a2009s.32k_fs_LR.dlabel.nii"
+destrieux_atlas_file_post = ".aparc.a2009s.32k_fs_LR.dlabel.nii"
 desikan_atlas_file_post = ".aparc.32k_fs_LR.dlabel.nii"
 
 #paths on aws machine
@@ -41,14 +44,30 @@ local_dir = external_hd_path + "/brain_data"
 
 #raw file location:
 # /hcp-openaccess/HCP_1200/100206/MNINonLinear/Results/rfMRI_REST1_RL/rfMRI_REST1_RL_Atlas_MSMAll_hp2000_clean.dtseries.nii
+############
+
 
 
 #which files to download
+# input: patient_id is a string of the patient id w/o any extra characters (e.g. '/')
 def list_files(patient_id):
     patient_id = str(patient_id)
-    files, readable_names = [], []
+
+    #list of dictionaries representing each file
     files = []
+
+    #path to directory for this suject
     subject_dir = local_dir+"/"+patient_id
+
+
+    ############
+    # for each file:
+    # hcp_path = path to file on the aws server
+    # readable_name = name to be used for printing
+    # local_path = path to file on local machine
+    ############
+
+    #CHANGE: include new block (see above) for a new file.
 
     #msall
     hcp_path = path2HCP_1200 + patient_id + lr_msmall_hcp_rel_path
@@ -73,23 +92,23 @@ def list_files(patient_id):
     files.append({"hcp_path": hcp_path, "readable_name": readable_name, "local_path": local_path})
 
 
-    hcp_path = path2HCP_1200 + patient_id + atlas_file_pre + patient_id + detrieux_atlas_file_post
+    #Destrieux atlas
+    hcp_path = path2HCP_1200 + patient_id + atlas_file_pre + patient_id + destrieux_atlas_file_post
     readable_name = "Destrieux_aparc32k"
-    local_path = subject_dir + "/" + patient_id + detrieux_atlas_file_post
+    local_path = subject_dir + "/" + patient_id + destrieux_atlas_file_post
     files.append({"hcp_path": hcp_path, "readable_name": readable_name, "local_path": local_path})
 
+    #Desikan atlas
     hcp_path = path2HCP_1200 + patient_id + atlas_file_pre + patient_id + desikan_atlas_file_post
     readable_name = "Desikan_aparc32k"
     local_path = subject_dir + "/" + patient_id + desikan_atlas_file_post
     files.append({"hcp_path": hcp_path, "readable_name": readable_name, "local_path": local_path})
-    
+
     return files
 
 
-#list of subject id's in HCP_1200
+#create list of string subject id's in HCP_1200. There are 1114 directories (each representing a patient)
 def subject_list_HCP_1200():
-    #HCP_1200 is 1114 directories, each of which represents a single subject
-    #return list of strings, each one representing the subject id of a subject
     args = ["aws s3 ls s3://hcp-openaccess/HCP_1200/"]
     completed_process = subprocess.run(args, capture_output=True, shell=True)
     all_dirs = completed_process.stdout
@@ -112,9 +131,11 @@ def save_subject_list_to_mat():
     subject_list = subject_list_HCP_1200()
     savemat("Allprocessedid.mat", {'subjList': subject_list})
 
-#for each patient, create a local diretory in local_dir and download all files. If there is an issue, write in log file.
+
+
+#for each patient, create a local diretory in local_dir and download all files. If there is an issue downloading, print to terminal and record which patient could not download each file
 def download_files():
-    
+
     #record who is missing which files
     files = list_files('000')
     patients_missing_file = {f["readable_name"]:set() for f in files}
@@ -126,86 +147,74 @@ def download_files():
         #create directory with this subject if it does not exist:
         subject_dir = local_dir+"/"+subject
         Path(subject_dir).mkdir(parents=True, exist_ok=True) #will ignore if exists
-        
+
 
         #download all files into this dir
         start = time.time()
         for f in list_files(subject):
-            #check if file already exists in directory
-            #if "no_msmall"  in f["readable_name"]:
-            #    input(f'no_small: {f["local_path"]} exist?: {os.path.isfile(f["local_path"])}')
-            #elif "msmall"  in f["readable_name"]:
-            #    input(f'msmall: {f["local_path"]} exist?: {os.path.isfile(f["local_path"])}')
 
             if os.path.isfile(f['local_path']):
                 print(f'\t{f["readable_name"]} already exists, skipping...')
                 continue
-            
+
             #if file does not already exist in local directory, attempt to download it
             print(f'\tdownloading {f["readable_name"]} into {subject_dir}...',end='')
             args = ["aws s3 cp s3:/" + f["hcp_path"] + " " + subject_dir]
             completed_process = subprocess.run(args, capture_output=True, shell=True)
-            
-            #if download did not complete properly, put this in log
+
+            #if download did not complete properly, print this and save this info
             if completed_process.returncode != 0:
                 print(f'ERROR (likely not on server or faulty path given')
                 patients_missing_file[f["readable_name"]].add(subject)
-                """
-                with open(log_file,'a+') as f:
-                    f.write(f"\n\n==={idx}th subject: {subject}===\n")
-                    f.write(str(datetime.datetime.now())+"\n")
-                    f.write(f"Error in downloading {file_to_download}\n")
-                    print(f"\tError in downloading {file_to_download}\n")
-                """
             else:
                 print(f'success')
 
         end = time.time()
         print(f"Time for download: {(end-start):.1f}")
-        
-        #Summarize whats missing
+
+        #Summarize whats missing: CHANGE - include new files here
         lr_msmall_miss = patients_missing_file["LR_msmall"]
         rl_msmall_miss = patients_missing_file["RL_msmall"]
         both_msmall_miss = lr_msmall_miss.intersection(rl_msmall_miss)
         lr_len, rl_len, both_len= len(lr_msmall_miss), len(rl_msmall_miss), len(both_msmall_miss)
         print(f"MSMALL missing:    lr {lr_len} | rl {rl_len} | both {both_len}")
-        
+
         lr_no_msmall_miss = patients_missing_file["LR_no_msmall"]
         rl_no_msmall_miss = patients_missing_file["RL_no_msmall"]
         both_no_msmall_miss = lr_no_msmall_miss.intersection(rl_no_msmall_miss)
         lr_len,rl_len,both_len=len(lr_no_msmall_miss),len(rl_no_msmall_miss), len(both_no_msmall_miss)
         print(f"NO_MSMALL missing: lr {lr_len} | rl {rl_len} | both {both_len}")
-    
 
-    #End of Download Summary
+
+    #End of Download Summary: CHANGE - include new files here
     print(f"======END OF DOWNLOAD=======")
     print(f"Total Patients: {len(subject_list)}")
     lr_msmall_miss, rl_msmall_miss = patients_missing_file["LR_msmall"],patients_missing_file["RL_msmall"]
     both_msmall_miss = lr_msmall_miss.intersection(rl_msmall_miss)
     lr_len, rl_len, both_len= len(lr_msmall_miss), len(rl_msmall_miss), len(both_msmall_miss)
     print(f"MSMALL missing:    lr {lr_len} | rl {rl_len} | both {both_len}")
-    
+
     lr_no_msmall_miss = patients_missing_file["LR_no_msmall"]
     rl_no_msmall_miss = patients_missing_file["RL_no_msmall"]
     both_no_msmall_miss = lr_no_msmall_miss.intersection(rl_no_msmall_miss)
     lr_len,rl_len,both_len=len(lr_no_msmall_miss),len(rl_no_msmall_miss), len(both_no_msmall_miss)
     print(f"NO_MSMALL missing: lr {lr_len} | rl {rl_len} | both {both_len}")
-    
+
     print(f"DESIKAN missing: {len(patients_missing_file['Desikan_aparc32k'])}")
     print(f"DESTRIE missing: {len(patients_missing_file['Destrieux_aparc32k'])}")
     print(f"======END OF DOWNLOAD=======")
-    
+
     print(f"List of patients with missing files")
     print(f"MSMALL")
     print(f"LR: \n{lr_msmall_miss}")
     print(f"RL: \n{rl_msmall_miss}")
     print(f"BOTH: \n{both_msmall_miss}")
-    
+
     print(f"\nNO MSMALL")
     print(f"LR: same as msmall lr? {lr_msmall==lr_no_small} \n{lr_msmall_miss}")
     print(f"RL: same as msmall rl? {rl_msmall==rl_no_small} \n{rl_no_msmall_miss}")
     print(f"BOTH: same as msmall both? {both_msmall_miss==both_no_msmall_miss} \n{both_no_msmall_miss}")
-    
+
     print(f"\nDESIKAN missing: {patients_missing_file['Desikan_aparc32k']}")
     print(f"DESTRIE missing: {patients_missing_file['Destrieux_aparc32k']}")
 
